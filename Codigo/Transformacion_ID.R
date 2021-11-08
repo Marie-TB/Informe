53ae6a6acfd7c65631b9ad40fbbe4d0f377d19c1
Mariella Tognarelli Buono-core <mariella.tognarelli@uc.cl>
2021-11-08 02:09:42 -0300

    Modificacion limpieza base y grafico principal

--- Codigo/Transformacion_ID.R
+++ Codigo/Transformacion_ID.R
@@ -2,6 +2,7 @@
 
 datos <- load("Datos/Datos_Filtados.Rdata")
 Autores <- rio::import("Datos/authors.csv")
+Categorias <- rio::import("Datos/categories.csv")
 
 # Cargar Librerías --------------------------------------------------------
 #nota ver librería regex
@@ -16,7 +17,6 @@ for (i in 1:length(datos3$authors)) {
     stringr::str_split(",")
   autor = c(autor, p)
 }
-length(autor[3])
 
 #### Cambio de ID por Nombres
 autores <- c()
@@ -40,6 +40,10 @@ for (i in autor){
       
     }
   }
+  else if (aux == "") {
+    nombres = "Anonimo"
+    autores_crudo = c(autores_crudo, nombres)
+  }
   else {
     posicion = which(i == Autores$author_id)
     nombres = Autores$author_name[posicion]
@@ -52,6 +56,9 @@ for (i in autor){
 save(autores, file="Datos/autores.Rdata")
 save(autores_crudo, file="Datos/autores_crudo.Rdata")
 
+
+###Dataframe
+
 contador_Autores <- plyr::count(autores_crudo)
 data_frame <- as.data.frame(autores_crudo) %>% 
   mutate(freq = NA)
@@ -60,12 +67,13 @@ for (i in 1:length(data_frame$autores_crudo)){
   data_frame$freq[i] = contador_Autores$freq[posicion]
 }
 
-datos3[1]
+###Histograma
 data_frame %>%
   filter(freq > 30) %>%
   ggplot(aes(autores_crudo))  +
   geom_bar(fill="thistle", col = "gray40") +
-  labs(x = "Autores", y = "Frecuencia", title = "Top 10 Autores con Mayor Cantidad de Best-seller")
+  labs(x = "Autores", y = "Frecuencia", title = "Los 11 Autores más nombrados en los 10.000 Top-Seller") +
+  ggthemes::theme_base()
 
 ###Se borra la columna Authors y se añade autores
 base_ajustada= datos3 %>% 
@@ -75,24 +83,66 @@ base_ajustada= datos3 %>%
 
 
 
+# Reemplazar ID de Categoría -------------------------------------------------
 
-
-
-# Reemplazar ID de Género -------------------------------------------------
-
-###SUJETO A CORRECCIÓN (Incompleto)
-colnames(datos3)[3] <- "category_id"
-Datos <- merge(datos3, generos)
-columna = c()
-for (dat in datos3[3]) {
-  data_genero = c()
-  for(cat in dat) {
-    a = which(generos[1] == cat)
-    data_genero = c(data_genero, generos[2][a,])
-  }
-  columna=c(columna, data_genero)
+categ <- c()
+for (i in 1:length(datos3$category_id)) {
+  p = datos3[i,3] %>% stringr::str_sub( start = 2L, end = -2L) %>%
+    stringr::str_remove_all(" ") %>%
+    stringr::str_split(",")
+  categ = c(categ, p)
 }
 
+#### Cambio de ID por nombres de Categoria
+categorias <- c()
+categorias_crudo <-c() 
+for (i in categ){
+  aux = unlist(i) 
+  if (length(aux) > 1){
+    nombre_ctg = ""
+    contador = 0
+    for (j in aux){
+      posicion = which(j == Categorias$category_id)
+      if (contador != 0){
+        nombre_ctg = paste(nombre_ctg, Categorias$category_name[posicion], sep = "; ")
+      }
+      else {
+        nombre_ctg = Categorias$category_name[posicion]
+        contador = 1
+      }
+      
+      categorias_crudo = c(categorias_crudo, Categorias$category_name[posicion])
+      
+    }
+  }
+  else if (aux == "") {
+    nombre_ctg = "Sin Especificar"
+    categorias_crudo = c(categorias_crudo, nombre_ctg)
+  }
+  else {
+    posicion = which(i == Categorias$category_id)
+    nombre_ctg = Categorias$category_name[posicion]
+    categorias_crudo = c(categorias_crudo, Categorias$category_name[posicion])
+    
+  }
+  categorias = c(categorias, nombre_ctg)
+}
 
+#Guardar listas#
+save(categorias, file="Datos/categorias.Rdata")
+save(categorias_crudo, file="Datos/categorias_crudo.Rdata")
 
+contador_Categorias <- plyr::count(categorias_crudo)
+data_frame <- as.data.frame(categorias_crudo) %>% 
+  mutate(freq = NA)
+for (i in 1:length(data_frame$categorias_crudo)){
+  posicion = which(data_frame$categorias_crudo[i] == contador_Categorias$x)
+  data_frame$freq[i] = contador_Categorias$freq[posicion]
+}
 
+###Histograma
+data_frame %>%
+  filter(freq > 360) %>%
+  ggplot(aes(categorias_crudo))  +
+  geom_bar(fill="thistle", col = "gray40") +
+  labs(x = "Categorías", y = "Frecuencia", title = "Top 10 Categorías con Mayor Cantidad de Best-seller")
