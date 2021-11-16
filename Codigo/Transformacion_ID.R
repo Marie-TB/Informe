81ad6976b11d03deba6dbd628bede342113e8a86
Mariella Tognarelli Buono-core <mariella.tognarelli@uc.cl>
2021-11-15 22:32:30 -0300

    Actualizacion

--- Codigo/Transformacion_ID.R
+++ Codigo/Transformacion_ID.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd02ecf76af9e882038e50fd1932174b28549088ca3a931dd276cdfc43220d66
-size 4156
+oid sha256:1489ca5d8381bfbc6a877c26b389cd3a2784be854f168ee0f00c23a5e5c7e527
+size 6290
